5e45f428db7a5f72b2555c0898df24e0b49fc8ad
Beve-hub <victorokeke89@gmail.com>
2026-09-15 13:02:43 +0100

    fixed refresh token

--- constant/authConstants.js
+++ constant/authConstants.js
@@ -3,6 +3,6 @@ module.exports = {
   REFRESH_COOKIE_NAME: "refresh_token",
   ACCESS_TOKEN_TTL_MS: 24 * 60 * 60 * 1000, // 24h
   ACCESS_TOKEN_TTL_JWT: "24h",
-  REFRESH_TOKEN_TTL_MS: 30 * 24 * 60 * 60 * 1000, // 30 days
+  REFRESH_TOKEN_TTL_MS: 24 * 60 * 60 * 1000, // 30 days
   AUTH_HEADER_PREFIX: "Bearer ",
 };
